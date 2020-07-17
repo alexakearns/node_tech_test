991d5db01fe3b459e08d3654b8f16f876ad1595f
Alexa <alexakearns@outlook.com>
2020-07-17 15:07:55 +0100

    Add data - array containing objects

---
+++ data.js
@@ -0,0 +1,32 @@
+let data = [
+  { "name": "John",
+    "age": "23",
+    "height": "180cm",
+    "value": '{ "date": "23-03-20", "location": "London" }'
+  },
+  { "name": "David",
+    "age": "34",
+    "height": "195cm",
+    "value": '{ "date": "13-05-20", "location": "Paris"}'
+  },
+  { "name": "Anna",
+  "age": "45",
+  "height": "160cm"
+  },
+  { "name": "Ben",
+    "age": "52",
+    "height": "192cm",
+    "value": '{ "date": "23-03-20", "location": "London" }'
+  },
+  { "name": "Mary",
+    "age": "35",
+    "height": "169cm"
+  },
+  { "name": "Clare",
+  "age": "64",
+  "height": "158cm",
+  "value": '{ "date": "19-04-20", "location": "Madrid" }'
+  }
+]
+
+module.exports = data
