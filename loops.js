0e92a4144aa06798455ee08d133f157c078349bf
Alexa <alexakearns@outlook.com>
2020-07-17 16:01:03 +0100

    Add second loop

--- loops.js
+++ loops.js
@@ -1,10 +1,28 @@
-const data = require('./data')
+const data = require("./data");
 
-data.forEach(function (obj) {
-  try {
-    console.log(JSON.parse(obj.value));
-  } catch (error) {
-    console.log('error')
+const loop1 = (dataSet) => {
+  dataSet.forEach(function (obj) {
+    try {
+      console.log(JSON.parse(obj.value));
+    } catch (error) {
+      console.log("error");
+    }
+  });
+};
+
+loop1(data);
+
+const loop2 = (dataSet) => {
+  for (let i = 0; i < dataSet.length; i++) {
+    let object = dataSet[i];
+    try {
+      console.log(JSON.parse(object.value));
+    } catch (error) {
+      console.log("error");
+    }
   }
-});
+};
 
+loop2(data);
+
+module.exports;
