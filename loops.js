9a640f604c6e7af6b37ac1be4c61d4dc8e945f3e
Alexa <alexakearns@outlook.com>
2020-07-17 16:54:38 +0100

    Add fourth loop fo parse value of objects

--- loops.js
+++ loops.js
@@ -10,7 +10,6 @@ const loop1 = (dataSet) => {
   });
 };
 
-
 const loop2 = (dataSet) => {
   for (let i = 0; i < dataSet.length; i++) {
     let object = dataSet[i];
@@ -22,11 +21,10 @@ const loop2 = (dataSet) => {
   }
 };
 
-
 const loop3 = (dataSet) => {
   let count = 0;
   const total = dataSet.length;
-  while (count <= total) {
+  while (count < total) {
     let object = dataSet[count];
     try {
       console.log(JSON.parse(object.value));
@@ -37,20 +35,18 @@ const loop3 = (dataSet) => {
   }
 };
 
+const loop4 = (dataSet) => {
+  for (let object of dataSet) {
+    try {
+      console.log(JSON.parse(object.value));
+    } catch (error) {
+      console.log("error");
+    }
+  }
+};
 
-// const loop4 = (dataSet) => {
-//   for (let object in dataSet) {
-//     try {
-//       console.log(JSON.parse(object.value));
-//     } catch (error) {
-//       console.log("error");
-//     }
-    
-//   }
-// }
-
-// loop1(data);
-// loop2(data);
+loop1(data);
+loop2(data);
 loop3(data);
-loop4(data)
+loop4(data);
 module.exports;
