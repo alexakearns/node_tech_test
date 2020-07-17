3be9410fb089df60e9ca3abb32870aafa7d1a11f
Alexa <alexakearns@outlook.com>
2020-07-17 16:05:42 +0100

    Add third loop

--- loops.js
+++ loops.js
@@ -25,4 +25,20 @@ const loop2 = (dataSet) => {
 
 loop2(data);
 
+const loop3 = (dataSet) => {
+  let count = 0;
+  const total = dataSet.length;
+  while (count <= total) {
+    count += 1;
+    let object = dataSet[count];
+    try {
+      console.log(JSON.parse(object.value));
+    } catch (error) {
+      console.log("error");
+    }
+  }
+};
+
+loop3(data);
+
 module.exports;
