a651fd07ecd7f2afb8caab162b5b493f37bbcd5c
Alexa <alexakearns@outlook.com>
2020-07-20 18:51:32 +0100

    Format document, remove comment

--- loops.js
+++ loops.js
@@ -1,20 +1,16 @@
 const data = require("./data");
-// const server = require('./server')
-const http = require('http');
-
-
+const http = require("http");
 
 const loop1 = (dataSet) => {
   dataSet.forEach(function (object) {
     processData(object);
   });
-}
-  
-  
-  const loop2 = (dataSet) => {
-    for (let i = 0; i < dataSet.length; i++) {
+};
+
+const loop2 = (dataSet) => {
+  for (let i = 0; i < dataSet.length; i++) {
     let object = dataSet[i];
-    processData(object)
+    processData(object);
   }
 };
 
@@ -23,14 +19,14 @@ const loop3 = (dataSet) => {
   const total = dataSet.length;
   while (count < total) {
     let object = dataSet[count];
-    processData(object)
+    processData(object);
     count += 1;
   }
 };
 
 const loop4 = (dataSet) => {
   for (let object of dataSet) {
-    processData(object)
+    processData(object);
   }
 };
 
@@ -39,45 +35,43 @@ const processData = (obj) => {
     let objVal = JSON.parse(obj.value);
     let image = objVal.img;
 
-    let body = image
+    let body = image;
 
-        let options = {
-        hostname: "localhost",
-        port: 3000,
-        method: "POST",
-        headers: {
-          "Content-Type": "text/plain",
-          "Content-Length": Buffer.byteLength(body)
-        }
-      }
-      
-      let data = ""
-      http
-      .request(options, res => {
-          res.on("data", d => {
-            data += d
-          })
-          res.on("end", () => {
-            console.log(data)
-          })
-        })
-        .on("error", console.error)
-        .end(body)
-        
-        let imgInfo = JSON.parse(data)
-        console.log(`Image ${image} is ${imgInfo.width}px x ${imgInfo.height}px and ${imgInfo.size_KB}KB.`)
+    let options = {
+      hostname: "localhost",
+      port: 3000,
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+        "Content-Length": Buffer.byteLength(body),
+      },
+    };
 
+    let data = "";
+    http
+      .request(options, (res) => {
+        res.on("data", (d) => {
+          data += d;
+        });
+        res.on("end", () => {
+          console.log(data);
+        });
+      })
+      .on("error", console.error)
+      .end(body);
+
+    let imgInfo = JSON.parse(data);
+    console.log(
+      `Image ${image} is ${imgInfo.width}px x ${imgInfo.height}px and ${imgInfo.size_KB}KB.`
+    );
   } catch (error) {
     console.log("error");
-  };
-}
+  }
+};
 
 loop1(data);
-// loop2(data);
-// loop3(data);
-// loop4(data);
-
-
-
+loop2(data);
+loop3(data);
+loop4(data);
 
 module.exports;
