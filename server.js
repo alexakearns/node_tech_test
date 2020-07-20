315cd41f63ba3260d6eaf84ff829de4b1c9655b6
Alexa <alexakearns@outlook.com>
2020-07-20 12:10:12 +0100

    Add server req, change resp.

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const http = require("http");
 
+
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World");
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", function (inputData) {
+      body += inputData;
+
+//check if file exists
+// throw err if not
+//return img properties - height/width/size
+
+
+    }); 
+
+    req.on("end", function(){
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(body);
+  });
+  }
 });
+
+
+
 
 server.listen(port, () => {
   console.log(`Node server running on port ${port}`);
