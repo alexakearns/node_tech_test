11771a6ff78627f27d52b3db14074be69a084338
Alexa <alexakearns@outlook.com>
2020-07-20 12:11:24 +0100

    Add fs and image-size modules. Image info to post

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require("http");
-
+const imageSize = require("image-size")
+const fs = require('fs')
 
 const port = 3000;
 
@@ -13,7 +14,15 @@ const server = http.createServer((req, res) => {
 // throw err if not
 //return img properties - height/width/size
 
+      const dimensions = imageSize('pink-clouds.png');
+      const width = dimensions.width
+      const height = dimensions.height
 
+      const stats = fs.statSync('pink-clouds.png')
+      const inKB = Math.round(stats["size"]/1000)
+      
+      body = `Image is ${width}px x ${height}px and ${inKB}KB.`
+      
     }); 
 
     req.on("end", function(){
